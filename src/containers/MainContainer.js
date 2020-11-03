3f267137aa698e5d4a9536b376182e89672a0657
github username <github email address>
2020-11-02 21:02:20 -0600

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,104 @@
 import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import StockContainer from './StockContainer';
+import PortfolioContainer from './PortfolioContainer';
+import SearchBar from '../components/SearchBar';
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    myStocks: [],
+    checked: '',
+    filter: '',
+    searchTerm: ''
+  };
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+      .then(res => res.json())
+      .then(stocks => {
+        console.log(stocks);
+        this.setState({ stocks });
+      });
+  }
+
+  handleStockClick = stock => {
+    this.state.myStocks.includes(stock)
+      ? null
+      : this.setState({
+          myStocks: [...this.state.myStocks, stock]
+        });
+  };
+
+  handleMyStockClick = stock => {
+    let myStocks = this.state.myStocks.filter(s => s.id !== stock.id);
+    this.setState({ myStocks });
+  };
+
+  handleCheck = e => {
+    let category = e.target.value;
+    switch (category) {
+      case 'Alphabetically':
+        return this.setState({ checked: 'Alphabetically' });
+      case 'Price':
+        return this.setState({ checked: 'Price' });
+    }
+  };
+
+  sortStocks = () => {
+    if (this.state.checked === 'Alphabetically') {
+      return this.state.stocks.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    } else if (this.state.checked === 'Price') {
+      return this.state.stocks.sort((a, b) => {
+        return a.price - b.price;
+      });
+    } else {
+      return this.state.stocks;
+    }
+  };
+
+  handleFilter = filter => {
+    this.setState({ filter });
+  };
+
+  handleChange = term => {
+    let searchTerm = term.toLowerCase();
+    this.setState({ searchTerm });
+  };
 
   render() {
+    let stocks = this.sortStocks().filter(stock =>
+      stock.type.includes(this.state.filter)
+    );
+    let searchedStocks = stocks.filter(stock =>
+      stock.name.toLowerCase().includes(this.state.searchTerm)
+    );
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          checked={this.state.checked}
+          handleCheck={this.handleCheck}
+          handleFilter={this.handleFilter}
+          handleChange={this.handleChange}
+        />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className='row'>
+          <div className='col-8'>
+            <StockContainer
+              stocks={searchedStocks}
+              handleClick={this.handleStockClick}
+            />
           </div>
+          <div className='col-4'>
+            <PortfolioContainer
+              stocks={this.state.myStocks}
+              handleClick={this.handleMyStockClick}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
